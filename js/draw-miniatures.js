2c1c2553084abd49ffc33b81cfd1fc999cd52ccc
AlexandraLeksy <_leksy_@list.ru>
2022-10-30 22:32:00 +0300

    7.16. Отрисуй меня полностью (часть 1)

--- js/draw-miniatures.js
+++ js/draw-miniatures.js
@@ -1,1 +1,34 @@
+// импортируем функцию, с которой будем работать
+import {allPhotos} from './util.js';
 
+const drawImages = function () {
+  // записываем в переменную блок для фотографий других пользователей
+  const photosContainer = document.querySelector('.pictures');
+  // шаблон нового элемента-ссылки
+  const elementTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+
+  // создание массива с фотографиями
+  const randomPhotos = allPhotos();
+
+  // добавление фрагмента
+  const photosContainerFragment = document.createDocumentFragment();
+
+  // проходимся по массиву для добавления данных
+  randomPhotos.forEach(({url, likes, comments}) => {
+  // клонирование шаблона
+    const randomPhotoElement = elementTemplate.cloneNode(true);
+    // адрес изображения url подставляем как атрибут src изображения.
+    randomPhotoElement.querySelector('.picture__img').src = url;
+    // количество лайков likes выводим в блок .picture__likes
+    randomPhotoElement.querySelector('.picture__likes').textContent = likes;
+    // количество комментариев comments выводим в блок .picture__comments
+    randomPhotoElement.querySelector('.picture__comments').textContent = comments.length;
+
+    photosContainerFragment.append(randomPhotoElement);
+  });
+
+  photosContainer.append(photosContainerFragment);
+};
+
+export {drawImages};
